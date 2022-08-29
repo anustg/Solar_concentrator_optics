9cebaa05f9ecf6a6598628f5a36d4747a539d95e
yewang726 <ye.wang@anu.edu.au>
2022-08-29 11:37:50 +1000

    initial setup

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
 # photogrammetry-processing
+
+# Introduction
+
+This repository contains Python scripts for photogremmetry ...
+
+(To be added)
+
+
+# Installation Instruction
+
+# Windows
+
+* For Windows (Windows 10, 64-bit).....
+
+## Linux
+
+* In Linux system .....
+
+
+# Some example syntax:
+
+* add a link: [link name](https://github.com/anustg/solstice-scripts/releases)
+
+* **bold**
+
+* icon for readthedocs: [![github](https://readthedocs.org/projects/solsticepy/badge/?version=latest)][1]
+
+* highlight text: `photopgrammetrypy`
+
+* example scripts:
+
+```bash
+### download this code repository:
+cd ~
+git clone https://github.com/anustg/solstice-scripts.git
+cd solstice-scripts
+### compile the post-processing scripts
+scons
+### install the post-processing scripts to the solstice PATH
+scons INSTALL_PREFIX=~/Solstice-0.9.0-GNU-Linux64 install
+```
+
+# References
+
+Markdown Guide: https://www.markdownguide.org/basic-syntax/
+
+
+[1]: https://solsticepy.readthedocs.io/en/latest/?badge=latest
+
+
+
+
+
---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(name='photogrammetrypy'
+	,version='0.0.1'
+	,author=''
+	,author_email=''
+	,description="Scripts for photogrammetry pre-and-post processing"
+	,long_description=long_description
+	,long_description_content_type="text/markdown"
+	,url='https://github.com/anustg/photogrammetry-processing'
+	,packages=setuptools.find_packages()
+	,license="GPL v3.0 or later, see LICENSE file"
+	,classifiers=[
+		"Development Status :: 4 - Beta"
+		,"Environment :: Console"
+		,"Intended Audience :: Science/Research"
+		,"License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)"
+		,"Natural Language :: English"
+		,"Operating System :: Microsoft :: Windows :: Windows 10"
+		,"Operating System :: POSIX :: Linux"
+		,"Programming Language :: Python :: 2"
+		,"Programming Language :: Python :: 3"
+		,"Topic :: Scientific/Engineering :: Physics"
+	]
+	,install_requires=['numpy']
+	,python_requires='>=2.7'
+)
